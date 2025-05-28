ed4711ece3b05b180c2a3c73e3aca6c9e76768a2
Camilo <camilogomezr25@gmail.com>
2025-05-28 09:49:13 -0500

    Iniciar el proyecto con SpringBoot

---
+++ src/main/java/IEComerce/Konrad/KonradApplication.java
@@ -0,0 +1,13 @@
+package IEComerce.Konrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonradApplication.class, args);
+	}
+
+}
